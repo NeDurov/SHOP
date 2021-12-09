6bfc389d14cb7db493ee090e3c084d2dd7ec06dc
NeDurov <maximabramov2000@yandex.ru>
2021-12-10 00:49:49 +0300

    Table(work)

--- model/formatter.js
+++ model/formatter.js
@@ -6,16 +6,35 @@ sap.ui.define(function () {
 		randomBoolean: function () {
 			return (Math.random() < 0.5);
 		},
+
 		statusText: function (sStatus) {
 
 			switch (sStatus) {
 				case "Available":
-					return 9;
+					return 1;
 				case "sold out":
 					return 3;
 				default:
 					return sStatus;
 			}
+		},
+
+		statusPrice: function (pPrice) {
+			if (pPrice > 550) {
+				return "Error";
+			} else { return "Success" }
+		},
+
+		statusWeight: function (wWeight, wWeight1) {
+			if (wWeight1 === "KG") {
+				if (wWeight > 10) {
+					return "Error";
+				} else {
+					if (wWeight < 1) {
+						return "Success";
+					}
+				}
+			} else { return "Success"; }
 		}
 	};
 
