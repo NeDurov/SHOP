659d05bd44d6d863aa80b4d9c7b5dd69160596b1
NeDurov <maximabramov2000@yandex.ru>
2021-12-14 20:46:11 +0300

    sort(work)

--- controller/InvoiceList.controller.js
+++ controller/InvoiceList.controller.js
@@ -3,8 +3,12 @@ sap.ui.define([
 	'sap/ui/core/mvc/Controller',
 	'sap/ui/model/json/JSONModel',
 	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-], function (formatter, Controller, JSONModel, Filter, FilterOperator) {
+	"sap/ui/model/FilterOperator",
+	'sap/ui/model/Sorter',
+	'sap/ui/core/Fragment',
+	'sap/ui/Device'
+
+], function (formatter, Controller, JSONModel, Filter, FilterOperator, Sorter, Fragment, Device) {
 	"use strict";
 
 	return Controller.extend("shop.controller.App", {
@@ -16,6 +20,7 @@ sap.ui.define([
 				currency: "EUR"
 			});
 			this.getView().setModel(oViewModel, "view");
+			this._mViewSettingDialog = {};
 		},
 
 		onFilterInvoices: function (oEvent) {
@@ -31,6 +36,46 @@ sap.ui.define([
 			var oList = this.byId("invoiceList");
 			var oBinding = oList.getBinding("items");
 			oBinding.filter(aFilter);
+		},
+
+		getViewSettingDialog: function (sDialogFragmentName) {
+			let pDialog = this._mViewSettingDialog[sDialogFragmentName];
+
+			if (!pDialog) {
+				pDialog = Fragment.load({
+					id: this.getView().getId(),
+					name: sDialogFragmentName,
+					controller: this
+				}).then(function (oDialog) {
+					if (Device.system.desktop) {
+						oDialog.addStyleClass("sapUiSizeCompact")
+					}
+					return oDialog;
+				});
+				this._mViewSettingDialog[sDialogFragmentName] = pDialog;
+			}
+			return pDialog;
+		},
+
+		handleSortButtonPressed: function () {
+			this.getViewSettingDialog("shop.view.SortInvoiceList").then(function (oViewSettingDialog) {
+				oViewSettingDialog.open();
+			});
+		},
+
+		handleSortDialogConfirm: function (oEvent) {
+			let oTable = this.byId("invoiceList"),
+				mParams = oEvent.getParameters(),
+				oBinding = oTable.getBinding("items"),
+				sPath,
+				bDescending,
+				aSorters = [];
+
+			sPath = mParams.sortItem.getKey();
+			bDescending = mParams.sortDescending;
+			aSorters.push(new Sorter(sPath, bDescending));
+
+			oBinding.sort(aSorters);
 		}
 	});
 
